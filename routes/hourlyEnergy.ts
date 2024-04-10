6ddd40ac524e63529880317effce8926a8a3cc78
Kazyel <m.mascarelo@gmail.com>
2024-04-10 20:48:15 -0300

    Code Organization

--- routes/hourlyEnergy.ts
+++ routes/hourlyEnergy.ts
@@ -1,82 +1,26 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
-import { prisma } from "../index.js";
+import { getHourlyEnergy } from "../utils/getHourlyEnergy.js";
 
 type DayRequest = FastifyRequest<{
-    Querystring: { day: string };
+  Querystring: { day: string };
 }>;
 
 const hourlyRoute = (
-    fastify: FastifyInstance,
-    options: never,
-    done: () => void
+  fastify: FastifyInstance,
+  options: never,
+  done: () => void
 ) => {
-    fastify.get("/api/hourly-energy", async (req: DayRequest) => {
-        const { day } = req.query;
+  fastify.get("/api/hourly-energy", async (req: DayRequest) => {
+    const { day } = req.query;
 
-        if (day) {
-            const initialDate = new Date(day).toISOString();
-            const limitDate = () => {
-                const baseDate = new Date(day);
-                const maxDate = baseDate.setHours(baseDate.getHours() + 23, 59);
-                const isoString = new Date(maxDate).toISOString();
-                return isoString;
-            };
+    if (day) {
+      const initialDate = new Date(day).toISOString();
+      return await getHourlyEnergy(initialDate, day);
+    }
 
-            const hourlyEnergy = await prisma.hourly_energy_in_day.findMany({
-                where: {
-                    createdAt: {
-                        gte: initialDate,
-                        lte: limitDate(),
-                    },
-                },
-                orderBy: {
-                    createdAt: "desc",
-                },
-            });
+    return await getHourlyEnergy();
+  });
 
-            const mostRecent = hourlyEnergy[0];
-
-            let labels = [];
-            if (mostRecent.energy) {
-                for (let i = 0; i < (mostRecent.energy as []).length; i++) {
-                    labels.push(i + "hrs");
-                }
-            }
-
-            const data = {
-                energy: mostRecent.energy,
-                labels: labels,
-            };
-            return data;
-        }
-
-        const hourlyEnergy = await prisma.hourly_energy_in_day.findMany({
-            where: {
-                createdAt: {
-                    // lte: date,
-                },
-            },
-            orderBy: {
-                createdAt: "desc",
-            },
-        });
-
-        const mostRecent = hourlyEnergy[0];
-
-        let labels = [];
-        if (mostRecent.energy) {
-            for (let i = 0; i < (mostRecent.energy as []).length; i++) {
-                labels.push(i + "hrs");
-            }
-        }
-
-        const data = {
-            energy: mostRecent.energy,
-            labels: labels,
-        };
-        return data;
-    });
-
-    done();
+  done();
 };
 export default hourlyRoute;
