ddbf4e9dfffa781213b142dc60d941634336fead
Kazyel <m.mascarelo@gmail.com>
2024-06-13 11:25:02 -0300

    Bug Correction and some refactoring

---
+++ Elena-Graphics-API/src/routes/homePanels.ts
@@ -0,0 +1,64 @@
+import { FastifyInstance } from 'fastify';
+import { Panels, homePanels as panels } from '../constants/panels.js';
+import { prisma } from '../db.js';
+import { JsonValue } from '@prisma/client/runtime/library';
+
+const homePanels = (
+    fastify: FastifyInstance,
+    options: never,
+    done: () => void
+) => {
+    fastify.get('/api/panels/home-panels', async (req, res) => {
+        /* 
+        Essa função faz uma busca no banco de dados para verificar se existe algum painel,
+        caso exista, ele pega o último painel cadastrado e retorna os dados do painel.
+
+        O painel é identificado pelo id_inv, que é o id do painel.
+        O painel é composto por dois painéis solares, panel-1 e panel-2.
+        Cada painel solar possui um id, x e y, que são as coordenadas do painel.
+        E por fim, a energia gerada pelo painel.
+    */
+
+        const panelsData = [];
+
+        for (const panel in panels) {
+            const panelObject = await prisma.home_panels.findFirst({
+                where: {
+                    id_inv: { contains: panels[panel].id },
+                },
+                orderBy: {
+                    createdAt: 'desc',
+                },
+            });
+
+            if (panelObject !== null) {
+                panelsData.push({
+                    id: panelObject.id_inv,
+                    panels: {
+                        'panel-1': {
+                            id: panelObject.positions![
+                                'panel-1' as keyof JsonValue
+                            ],
+                            x: panels[panel].panelsAxes['panel-1'].x,
+                            y: panels[panel].panelsAxes['panel-1'].y,
+                        },
+                        'panel-2': {
+                            id: panelObject.positions![
+                                'panel-2' as keyof JsonValue
+                            ],
+                            x: panels[panel].panelsAxes['panel-2'].x,
+                            y: panels[panel].panelsAxes['panel-2'].y,
+                        },
+                    },
+                    energy: panelObject.energy,
+                });
+            }
+        }
+
+        return panelsData;
+    });
+
+    done();
+};
+
+export default homePanels;
--- Elena-Graphics-API/src/server.ts
+++ Elena-Graphics-API/src/server.ts
@@ -1,33 +1,30 @@
-import Fastify from "fastify";
-import path from "node:path";
-import cors from "@fastify/cors";
+import path from 'node:path';
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
 
-import { fastifyStatic } from "@fastify/static";
-import { fileURLToPath, pathToFileURL } from "node:url";
+import laundryPanels from './routes/laundryPanels.js';
+import homePanels from './routes/homePanels.js';
+import restaurantPanels from './routes/restaurantPanels.js';
+import cabinetPanels from './routes/cabinetPanels.js';
 
-import hourlyRoute from "./routes/hourlyEnergy.js";
-import powerDayRoute from "./routes/realtimeEnergy.js";
-import dailyPeriodRoute from "./routes/dailyPeriod.js";
-import monthlyEnergyRoute from "./routes/monthlyEnergy.js";
-import yearlyEnergyRoute from "./routes/yearlyEnergy.js";
+import { fastifyStatic } from '@fastify/static';
+import { fileURLToPath, pathToFileURL } from 'node:url';
 
-// Configs
 export const fastify = Fastify();
 await fastify.register(cors, {});
 
 // Client
-const clientDir = fileURLToPath(pathToFileURL("public"));
+const clientDir = fileURLToPath(pathToFileURL('./public'));
 fastify.register(fastifyStatic, {
-  root: path.join(clientDir),
+    root: path.join(clientDir),
 });
 
-fastify.get("/", function (_, reply) {
-  reply.sendFile("./index.html");
+fastify.get('/', function (_, reply) {
+    reply.sendFile('./index.html');
 });
 
-// Energy Routes
-fastify.register(hourlyRoute);
-fastify.register(powerDayRoute);
-fastify.register(dailyPeriodRoute);
-fastify.register(monthlyEnergyRoute);
-fastify.register(yearlyEnergyRoute);
+// Panels routes
+fastify.register(cabinetPanels);
+fastify.register(restaurantPanels);
+fastify.register(homePanels);
+fastify.register(laundryPanels);
